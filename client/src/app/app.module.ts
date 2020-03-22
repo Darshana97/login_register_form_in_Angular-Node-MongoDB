f5f31edc8121f3796c078f298ef1f9025486b0be
Darshana Senevirathna <39029875+Darshana97@users.noreply.github.com>
2020-03-22 12:22:44 +0530

    register

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,8 +1,17 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {HttpClientModule} from '@angular/common/http';
+import {FormsModule} from '@angular/forms';
+import {RouterModule,Routes} from '@angular/router';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {ProfileComponent} from './profile/profile.component';
+import {LoginComponent} from './login/login.component';
+import {RegisterComponent} from './register/register.component';
+import {HomeComponent} from './home/home.component';
+import {AuthenticationService} from './authentication.service';
+import {AuthGuardService} from './auth-guard.service';
 
 @NgModule({
   declarations: [
